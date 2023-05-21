9900f4838b169f6acfcae5e82c6de63eb28d3c06
Luisgustavo11 <luisgustavordp@gmail.com>
2023-05-21 00:34:26 -0300

    Add comentario

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
+// tag section com o sobre mim
 const AboutContainer = () => {
     return (
         <section className="about-container">
             <h2>Sobre</h2>
             <p>Me chamo Luis Gustavo, tenho 28 anos, moro em SP e amo tecnologia, sou formado em Análise e desenvolvimento de sistemas, sempre busco cada vez mais conhecimento através de cursos especializados em programação e tecnologia no geral, estou me formando no curso de desenvolvimento Web da Trybe, para aprofundar e solidar meus aprendizados. Gosto muito de jogar fighting games, gosto de treinar na academia, sou fã da NBA e amo ficar junto da minha família.</p>
         </section>
-
     )
 }
 
