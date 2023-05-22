dcf92e68a3d072fc929d91a5c6f12cfe61fd168a
Luisgustavo11 <luisgustavordp@gmail.com>
2023-05-22 01:03:17 -0300

    Adicionando link aos incons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,24 +6,30 @@ import InformationContainer from "./InformationContainer";
 import SocialNetworks from "./SocialNetworks";
 
 const Sidebar = () => {
-    const handleScrollTop = () => {
-        window.scrollTo({top: 0, behavior: 'smooth'});
+   const handleScrollTop = () => {
+      window.scrollTo({top: 0, behavior: 'smooth'});
     }
 
-        const [imageLoaded, setImageLoaded] = useState(false);
+   const [imageLoaded, setImageLoaded] = useState(false);
       
-        const handleImageLoad = () => {
-          setImageLoaded(true);
-        };
+   const handleImageLoad = () => {
+      setImageLoaded(true);
+   };
+   return (
+     <aside id="sidebar">
 
-    return (
-        <aside id="sidebar">
-            <img src={Avatar} alt="Luis Gustavo" class="loaded" onLoad={handleImageLoad}
-        className={imageLoaded ? "loaded" : ""}/>
-            <p className="title">Desenvolvedor</p>
-            <SocialNetworks />
-            <InformationContainer />
-            <button className="btn" onClick={handleScrollTop}>Voltar ao topo</button>
+       <img 
+         src={Avatar} 
+         alt="Luis Gustavo" 
+         class="loaded" 
+         onLoad={handleImageLoad}
+         className={imageLoaded ? "loaded" : ""}
+       />
+       <p className="title">Desenvolvedor</p>
+         <SocialNetworks />
+         <InformationContainer />
+         <button 
+         className="btn" onClick={handleScrollTop}>Voltar ao topo</button>
         </aside>
     )
 }
--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -4,16 +4,36 @@ import { FaLinkedinIn, FaGithub} from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 
 const socialNetworks = [
-    { name: "linkedin", icon: <FaLinkedinIn /> },
-    { name: "github", icon: <FaGithub /> },
-    { name: "gmail", icon: <SiGmail /> },
+  {
+    name: "linkedin", 
+    icon: <FaLinkedinIn />, 
+    url: "https://www.linkedin.com/in/luis-gustavo-r-de-paula-45584320a/" 
+  },
+  { 
+    name: "github", 
+    icon: <FaGithub />,
+    url: "https://github.com/Luisgustavo11" 
+  },
+  { 
+    name: "gmail", 
+    icon: <SiGmail />,
+    url: "luisgustavordp@gmail.com",
+  },
   ];
 
 const SocialNetworks = () => {
-    return (
-        <section id="social-networks">
-            {socialNetworks.map((network) => (
-                <a href="#" className="social-btn" id={network.name} key={network.name}>
+  return (
+    <section id="social-networks">
+      {socialNetworks.map((network) => (
+
+         <a
+           href={network.url} 
+           className="social-btn" 
+           id={network.name} 
+           key={network.name}
+           target="_blank" 
+           rel="noopener noreferrer"
+          >
                     {network.icon}
                 </a>
             ))}
